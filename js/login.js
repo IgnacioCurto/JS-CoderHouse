39a2eda22bd3088204bfe4648dd6d9837d2d4df3
Ignacio Curto <ignacio.n.curto@gmail.com>
2021-08-29 12:49:21 -0300

    better header fix

--- js/login.js
+++ js/login.js
@@ -2,12 +2,25 @@ $(document).ready(function(){
 
     //Declaramos la url que vamos a usar para el GET
     const URLGET   = "https://jsonplaceholder.typicode.com/posts"
+
+    //Controlador para evitar múltiples login
+    var counter=0;
+    function a(){
+        if(counter > 1){
+            document.getElementById('btn1').innerHTML += "<input type='radio'>";
+            counter++;
+        }
+        else{
+            document.getElementById("btn1").disabled=true;
+        }
+    }
     //Declaramos la información a enviar
     const infoPost =  { nombre: "Ana Díaz", profesion: "Profesora en Ciencias Sociales" }
     //Escuchamos el evento click del botón agregado
     $("#btn1").click(() => { 
         $.post(URLGET, infoPost ,(respuesta, estado) => {
             if(estado === "success"){
+                a()
                 $(".modal-body").prepend(`
                 <h5 class="mt-1 mb-2">${respuesta.nombre}</h5>
                 <h6 class="text-secondary mt-1 mb-2">${respuesta.profesion}</h6>
@@ -15,7 +28,7 @@ $(document).ready(function(){
                 $(".loginBtn").hide();
                 $('.teacher').hide()
                 $('.modal-header').append(`<img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%281%29.jpg" alt="avatar" class="rounded-circle img-responsive"></img> `)
-                $('.header').append(`<button class="btn btn-dark btn-rounded align-self-end">Log out</button>`)
+                $('.btndiv').append(`<button class="btn bg-transparent btn-rounded border border-white text-white" id="btn2">Log out</button>`)
             }  
         });
     });
